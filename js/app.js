1518e25eab4ed9df7bb8e5490f652e714534aa44
Marina Zujic <zujicmarina@gmail.com>
2020-07-21 17:33:09 +0200

    Js changed, overlay displays dinamically

--- js/app.js
+++ js/app.js
@@ -1,28 +1,46 @@
-const navOverlay = document.querySelector('.nav__overlay');
-
-navButton.addEventListener('click', function() {
-
-  navMenu.classList.toggle('nav__menu--open');
-  navButton.classList.toggle('nav__button--open');
+const nav = document.getElementById('nav');
+const navMenu = document.getElementById('navMenu');
+const navButton = document.getElementById('navButton');
+const navOverlay = document.querySelector('.hidden-overlay');
 
 
-  if (nav.classList.contains('nav--open')) {
-    nav.classList.remove('nav--open');
+navButton.addEventListener('click', function () {
+  openNavigation();
+  openButton();
+});
 
-    navOverlay.classList.add('fade-out');
-    navOverlay.classList.remove('fade-in');
 
-    navMenu.classList.add('fade-out');
-    navMenu.classList.remove('fade-in');
+let menuOpen = false;
+function openNavigation() {
+  if (!menuOpen) {
+    navMenu.classList.add('nav__menu--open');
+    navOverlay.classList.add('nav__overlay','fade-in');
+    navMenu.classList.add('fade-in');
+    menuOpen = true;
 
   } else {
-    nav.classList.add('nav--open');
+    navMenu.classList.add('fade-out');
+    navOverlay.classList.add('fade-out');
 
-    navOverlay.classList.add('fade-in')
-    navOverlay.classList.remove('fade-out');
+    navMenu.classList.remove('fade-in');
+    navOverlay.classList.remove('fade-in');
 
-    navMenu.classList.add('fade-in')
-    navMenu.classList.remove('fade-out');
+    setTimeout(() => {
+      navMenu.classList.remove('nav__menu--open');
+      navOverlay.classList.remove('fade-out');
+      navMenu.classList.remove('fade-out');
+    }, 1000);
+    menuOpen = false;
   }
+}
 
-});
+let buttonOpen = false;
+function openButton() {
+  if (!buttonOpen) {
+    navButton.classList.add('nav__button--open');
+    buttonOpen = true;
+  } else {
+    navButton.classList.remove('nav__button--open');
+    buttonOpen = false;
+  }
+}
